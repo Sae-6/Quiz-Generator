81bd6d76e44ab02a116fc0f9c9b7b7a36e94a7e0
Sae-6 <evansseamus35@gmail.com>
2024-09-30 11:37:29 -0400

    First question

---
+++ Quiz.py
@@ -0,0 +1,8 @@
+#SeamusEvans-Sept29th-Quiz Generator 
+U_input=int(input("What was the release year of Kings Field? \n").strip())
+if U_input==1994: 
+    print ("Correct")
+else:
+    print("incorrect")
+
+
