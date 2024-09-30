dfe133829370e9cdb9c820d3c8548edf3e8723c5
Sae-6 <evansseamus35@gmail.com>
2024-09-30 12:08:19 -0400

    first 3 done

--- Quiz.py
+++ Quiz.py
@@ -1,8 +1,17 @@
 #SeamusEvans-Sept29th-Quiz Generator 
-U_input=int(input("What was the release year of Kings Field? \n").strip())
+U_input=int(input("Question 1- \nWhat was the release year of Kings Field? \n").strip())
 if U_input==1994: 
     print ("Correct")
 else:
     print("incorrect")
+U_input2=int(input("Question 2- \n When was Armored Core released? \n").strip())
 
-
+if U_input2==1997:
+    print ("Correct")
+else:
+    print("incorrect")
+    U_input3=input("Question 3- \n What was Fromsoftwares 2009 hit title named? \n").lower()
+if U_input3 =="demon's souls":
+    print ("correct")
+else:
+    print("incorrect")
